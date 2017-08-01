99579107f7d8113b4bf8550e1d1b199f520fb032
SINA <sinahamidi1@gmail.com>
2017-08-02 01:24:45 +0430

    commit #2

---
+++ app/src/main/java/app/hamidi/siavash/abzums/pushup/MainActivity.java
@@ -0,0 +1,19 @@
+package app.hamidi.siavash.abzums.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void testPush(){
+        int i = 1+1;
+    }
+
+
+}
