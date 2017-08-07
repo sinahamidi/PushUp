cb928335fd84e3df59db3b1566626b2500d60e86
SINA <sinahamidi1@gmail.com>
2017-08-07 23:41:27 +0430

    commit #4

--- app/src/main/java/app/hamidi/siavash/abzums/pushup/MainActivity.java
+++ app/src/main/java/app/hamidi/siavash/abzums/pushup/MainActivity.java
@@ -1,7 +1,7 @@
 package app.hamidi.siavash.abzums.pushup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +11,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void testPush(){
-        int i = 2+2;
+    public void testPush() {
+        int i = 2 + 2;
+    }
+
+    public void testBranch() {
+        String s = "ss";
     }
 
 
